82c63406848e1660ba8c900a85414d3ac504b188
Sa'ad <patelsaad39@gmail.com>
2021-06-29 12:28:31 +0530

    sidebar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
     return (
         <div className="header">
             <div className="header__left">
-                <img src="https://image.flaticon.com/icons/png/512/174/174857.png" alt="linkedin_icon" />
+                <img src="/linkedin-icon.svg" />
                 
                 <div className="header__search">
                     <SearchIcon />
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Avatar } from '@material-ui/core';
+import BookmarkSharpIcon from '@material-ui/icons/BookmarkSharp';
+import '../styles/Sidebar.scss';
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <div className="siderbar__top">
-                <img src="" alt="" />
-                <Avatar />
+            <div className="sidebar__top">
+                <img className="sidebar__coverPhoto" src="https://media-exp3.licdn.com/dms/image/C4D16AQE1AJrtk4-MRQ/profile-displaybackgroundimage-shrink_350_1400/0/1623083809608?e=1630540800&v=beta&t=X4tBbzMdeQouKtJLgV1vfOxMZg_UhJCTtO8Cc853PPs" alt="Cover_Picture" />
+                <Avatar className="sidebar__avatar" src="https://avatars.githubusercontent.com/u/62077144?s=400&u=d53b191db878c5f288358fbaffe0a6733ec06c1e&v=4" />
                 <h2>Saad Patel</h2>
                 <h4>patelsaad39@gmail.com</h4>
             </div>
+
+            <hr />
 
             <div className="sidebar__stats">
                 <div className="sidebar_stat">
@@ -22,8 +26,11 @@ const Sidebar = () => {
                 </div>
             </div>
 
-            <div className="sidebar__recent">
-                <p>Recent</p>
+            <hr />
+            
+            <div className="sidebar__myItems">
+                <BookmarkSharpIcon />
+                <p>My Items</p>
             </div>
         </div>
     )
